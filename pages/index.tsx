78e383be03d13a700a3082be4796525dabfa0de9
441222 <6120nashville@gmail.com>
2024-07-09 10:46:03 +0900

    add ogp

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,12 +9,12 @@ const Home: React.FC = () => {
         <meta name="description" content="东京电讯(Tokyo Teleport)" />
         <meta property="og:title" content="东京电讯(Tokyo Teleport)" />
         <meta property="og:description" content="东京电讯(Tokyo Teleport)" />
-        <meta property="og:image" content="public\logo2.png" />
+        <meta property="og:image" content="/logo2.png" />
         <meta property="og:url" content="https://tokyo-teleport.com" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="东京电讯(Tokyo Teleport)" />
         <meta name="twitter:description" content="东京电讯(Tokyo Teleport)" />
-        <meta name="twitter:image" content="public\logo2.png" />
+        <meta name="twitter:image" content="/logo2.png" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
